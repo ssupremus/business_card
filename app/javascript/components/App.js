4de0dc121bbcb4992549ffa1ae12afd8fcb3661e
Yurii Sushkov <s.supremus@gmail.com>
2020-12-20 18:24:18 +0200

    AppBar and SideBar added.

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import MenuAppBar from './NavBar'
+// import Airlines from './Airlines/Airlines'
+// import Airline from './Airline/Airline'
+
+const App = () => {
+  return (
+    <MenuAppBar />
+    )
+    // <Switch>
+    // </Switch>
+    // {/* <Route exact path="/" component={Airlines}/> */}
+    // {/* <Route exact path="/airlines/:slug" component={Airline}/> */}
+}
+
+export default App
